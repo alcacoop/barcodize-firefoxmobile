dd45f166eff8b125435b893e9ce3245de0d93a11
Luca Greco <luca.greco@alcacoop.it>
2010-11-07 20:49:50 +0100

    FIX: get current tab url to work on firefox mobile 4.0b2

--- chrome/content/overlayFennec.js
+++ chrome/content/overlayFennec.js
@@ -1,7 +1,6 @@
 function barcodize_opentab() {
-  //var barcode_tab = Browser.addTab('http://chart.apis.google.com/chart?cht=qr&chld=M|4&choe=UTF-8&chs=300x300&chl='+escape(Browser.selectedTab.browser.contentWindow.location.href)); 
   var barcode_tab = Browser.addTab('chrome://barcodize/content/barcodize.html#'+                               
-                                   escape(content.location.href));
+				   escape(Browser.selectedTab.browser.currentURI.spec));
   Browser.selectedTab = barcode_tab;
   Browser.hideSidebars();
 }
